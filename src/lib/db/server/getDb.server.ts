a20b5d322d9ae13913b673b92baf98ccd0989a20
Kim Roberts <kimr@apple.com>
2025-07-13 21:32:52 -0700

    fix: `neon` connection

--- src/lib/db/server/getDb.server.ts
+++ src/lib/db/server/getDb.server.ts
@@ -1,5 +1,6 @@
 import * as schema from '../tables'
-import { drizzle } from 'drizzle-orm/postgres-js'
+import { drizzle as neon } from 'drizzle-orm/neon-http'
+import { drizzle as local } from 'drizzle-orm/postgres-js'
 
 const { DATABASE_LOG, DATABASE_URL } = process.env
 
@@ -7,13 +8,16 @@ if (!DATABASE_URL) {
 	throw Error('DATABASE_URL is not defined in .env')
 }
 
-const db = drizzle({
+const isLocal = DATABASE_URL.includes('localhost')
+const config = {
 	connection: {
 		url: DATABASE_URL
 	},
 	schema,
 	logger: DATABASE_LOG === 'true'
-})
+}
+
+const db = isLocal ? local(config) : neon(DATABASE_URL)
 
 export function getDb() {
 	return db
